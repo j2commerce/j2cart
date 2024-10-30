8900713575d89126e09ab0b5abc131b249196a61
J2Commerce <olivier@j2commerce.com>
2024-10-29 20:34:54 -0400

    Copyright update

--- administrator/components/com_j2store/views/products/tmpl/default_items.php
+++ administrator/components/com_j2store/views/products/tmpl/default_items.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package J2Store
- * @copyright Copyright (c)2014-17 Ramesh Elamathi / J2Store.org
- * @copyright Copyright (c) 2024 J2Commerce
- * @license GNU GPL v3 or later
+ * @copyright Copyright (C) 2014-2019 Weblogicx India. All rights reserved.
+ * @copyright Copyright (C) 2024 J2Commerce, Inc. All rights reserved.
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or later
+ * @website https://www.j2commerce.com
  */
 // No direct access
 defined('_JEXEC') or die;
