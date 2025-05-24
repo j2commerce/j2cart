369ceea724abe7d76ab2851e493a38698cfda263
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-05-23 20:25:37 -0400

    Updated link to site and doc

--- administrator/components/com_j2store/views/shippings/tmpl/shipping_info.php
+++ administrator/components/com_j2store/views/shippings/tmpl/shipping_info.php
@@ -1,13 +1,16 @@
 <?php
 /**
- * @package J2Store
- * @copyright Copyright (c)2014-17 Ramesh Elamathi / J2Store.org
- * @copyright Copyright (c) 2024 J2Commerce . All rights reserved.
- * @license GNU GPL v3 or later
+ * @package     Joomla.Administrator
+ * @subpackage  com_j2store
+ *
+ * @copyright Copyright (C) 2014-2019 Weblogicx India. All rights reserved.
+ * @copyright Copyright (C) 2025 J2Commerce, LLC. All rights reserved.
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or later
+ * @website https://www.j2commerce.com
  */
 
-// No direct access to this file
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
@@ -30,7 +33,7 @@ if (version_compare(JVERSION, '3.99.99', 'lt')) {
                 </div>
 
                 <div class="text-center mt-auto mb-4">
-                    <a class="btn btn-outline-primary app-button-open" href="<?php echo J2Store::buildHelpLink('support/shipping-methods', 'shipping'); ?>" target="_blank"><span class="fas fa-solid fa-arrow-up-right-from-square me-2"></span><?php echo Text::_('J2STORE_SHIPPING_HELP_BUTTON_GUIDE_LABEL'); ?></a>
+                    <a class="btn btn-outline-primary app-button-open" href="<?php echo J2Store::buildHelpLink('shipping-methods', 'shipping'); ?>" target="_blank"><span class="fas fa-solid fa-arrow-up-right-from-square me-2"></span><?php echo Text::_('J2STORE_SHIPPING_HELP_BUTTON_GUIDE_LABEL'); ?></a>
                     <a class="btn btn-primary app-button-open" href="<?php echo Route::_('index.php?option=com_j2store&view=shippingtroubles'); ?>" target="_blank"><?php echo Text::_('COM_J2STORE_TITLE_SHIPPINGTROUBLES'); ?></a>
                 </div>
             </div>
@@ -44,7 +47,7 @@ if (version_compare(JVERSION, '3.99.99', 'lt')) {
                 </div>
 
                 <div class="text-center mt-auto mb-4">
-                    <a class="btn btn-outline-primary app-button-open" href="<?php echo J2Store::buildHelpLink('extensions/shipping-plugins', 'shipping'); ?>" target="_blank"><span class="fas fa-solid fa-arrow-up-right-from-square me-2"></span><?php echo Text::_('J2STORE_SHIPPING_APPS_BUTTON_LABEL'); ?></a>
+                    <a class="btn btn-outline-primary app-button-open" href="<?php echo J2Store::buildSiteLink('extensions/shipping-plugins', 'shipping'); ?>" target="_blank"><span class="fas fa-solid fa-arrow-up-right-from-square me-2"></span><?php echo Text::_('J2STORE_SHIPPING_APPS_BUTTON_LABEL'); ?></a>
                 </div>
             </div>
 		</div>
