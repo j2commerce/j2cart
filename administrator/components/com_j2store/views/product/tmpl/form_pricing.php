aca38638839d45dc7666719767b7d6961e3e1b66
J2Commerce <olivier@j2commerce.com>
2025-08-18 16:20:41 -0400

    [4.0.17] Miscellaneous fixes (#236)
    * Added J2StoreSubmitbuttonOverride function to allow compatibility with other third parties
    
    * Use relative variant images to allow external links
    
    * Allow non-shipping items at checkout when mandatory shipping is enabled
    
    * Fix to ensure API calls do not fail
    
    * Updated version
    
    * Added events at strategic locations
    Updated UI
    
    * Updated UI
    
    * Updated version
    
    * Added message keys to create options if none exist
    
    * Namespaces use
    
    * Added text-danger class to delete icon
    
    * Minor UX changes
    Fixed pagination limit
    
    * Namespaces use
    
    * Minor UX changes
    
    * Major UX changes
    
    * Test on options and variant counts
    
    * Additional check to prevent division by zero error
    
    * Updated date
    
    ---------
    
    Co-authored-by: Olivier Buisard <olivier.buisard@simplifyyourweb.com>

--- administrator/components/com_j2store/views/product/tmpl/form_pricing.php
+++ administrator/components/com_j2store/views/product/tmpl/form_pricing.php
@@ -1,51 +1,55 @@
 <?php
 /**
- * @package J2Store
- * @copyright Copyright (c)2014-17 Ramesh Elamathi / J2Store.org
- * @license GNU GPL v3 or later
+ * @package     Joomla.Component
+ * @subpackage  J2Store
+ *
+ * @copyright Copyright (C) 2014-24 Ramesh Elamathi / J2Store.org
+ * @copyright Copyright (C) 2025 J2Commerce, LLC. All rights reserved.
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or later
+ * @website https://www.j2commerce.com
  */
 
-// No direct access
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
 //pricing options
 $variant_pricing_calculator = (isset($this->variant->pricing_calculator))?$this->variant->pricing_calculator :'';
 $pricing_calculator = J2Html::select()->clearState()
-->type('genericlist')
-->name($this->form_prefix.'[pricing_calculator]')
-->value($variant_pricing_calculator)
-->setPlaceHolders(J2Store::product()->getPricingCalculators())
-->getHtml();
-$base_path = rtrim(JUri::root(),'/').'/administrator';
+    ->type('genericlist')
+    ->name($this->form_prefix.'[pricing_calculator]')
+    ->value($variant_pricing_calculator)
+    ->setPlaceHolders(J2Store::product()->getPricingCalculators())
+    ->getHtml();
+$base_path = rtrim(Uri::root(),'/').'/administrator';
 ?>
-<?php if (version_compare(JVERSION, '3.99.99', 'lt')): ?>
-<style>
-    .fancybox-slide--iframe .fancybox-content {
-        width: 1000px !important;
-        height: 600px !important;
-    }
-</style>
-<?php endif; ?>
 <div class="j2store-product-pricing">
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_REGULAR_PRICE'), 'price' ,array('class'=>'control-label')); ?>
-		<?php echo J2Html::price($this->form_prefix.'[price]',(isset($this->variant->price))? $this->variant->price:'', array('class'=>'input')); ?>
-	</div>
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_SET_ADVANCED_PRICING'), 'sale_price',array('class'=>'control-label')); ?>
-		<!-- Link to advanced pricing options. Opens as a popup. -->
-        <a data-fancybox class="btn btn-success" data-type="iframe" data-src="<?php echo $base_path."/index.php?option=com_j2store&view=products&task=setproductprice&variant_id=".$this->variant->j2store_variant_id."&layout=productpricing&tmpl=component";?>" href="javascript:;">
-            <?php echo JText::_( "J2STORE_PRODUCT_SET_PRICES" );?>
-        </a>
-		<?php //echo J2StorePopup::popup( $base_path."/index.php?option=com_j2store&view=products&task=setproductprice&variant_id=".$this->variant->j2store_variant_id."&layout=productpricing&tmpl=component", JText::_( "J2STORE_PRODUCT_SET_PRICES" ), array('class'=>'btn btn-success'));?>
-	</div>
-	<div class="control-group">
-		<?php echo J2Html::label(JText::_('J2STORE_PRODUCT_PRICING_CALCULATOR'), 'price_calculator',array('class'=>'control-label')); ?>
-		<?php //dropdown list: pre-populate it with Standard (to start with). We will extend this at a later point of time ?>
-		<?php echo $pricing_calculator;?>
-	</div>
+    <fieldset class="options-form">
+        <legend><?php echo Text::_('J2STORE_PRODUCT_TAB_PRICE');?></legend>
+        <div class="form-grid">
+            <div class="control-group">
+                <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_REGULAR_PRICE'), 'price'); ?></div>
+                <div class="controls">
+                    <?php echo J2Html::price($this->form_prefix.'[price]',(isset($this->variant->price))? $this->variant->price:'', array('class'=>'form-control')); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_SET_ADVANCED_PRICING'), 'sale_price'); ?></div>
+                <div class="controls">
+                    <a data-fancybox class="btn btn-success" data-type="iframe" data-src="<?php echo $base_path."/index.php?option=com_j2store&view=products&task=setproductprice&variant_id=".$this->variant->j2store_variant_id."&layout=productpricing&tmpl=component";?>" href="javascript:;">
+                        <?php echo Text::_('J2STORE_PRODUCT_SET_PRICES');?>
+                    </a>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="control-label"><?php echo J2Html::label(Text::_('J2STORE_PRODUCT_PRICING_CALCULATOR'), 'price_calculator'); ?></div>
+                <div class="controls"><?php echo str_replace('<select', '<select class="form-select"', $pricing_calculator); ?></div>
+            </div>
+        </div>
+    </fieldset>
 </div>
-
-<div class="alert alert-info">
-<h4><?php echo JText::_('J2STORE_QUICK_HELP'); ?></h4>
-<?php echo JText::_('J2STORE_PRODUCT_PRICE_HELP_TEXT'); ?>
+<div class="alert alert-info mt-3 mb-0">
+    <h4 class="alert-heading"><?php echo Text::_('J2STORE_QUICK_HELP'); ?></h4>
+    <?php echo Text::_('J2STORE_PRODUCT_PRICE_HELP_TEXT'); ?>
 </div>
