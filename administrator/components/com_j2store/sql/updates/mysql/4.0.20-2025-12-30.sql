b999713a7c712261d9c4449924af0bec83287158
Olivier <olivier.buisard@simplifyyourweb.com>
2026-01-17 20:16:47 -0500

    Updated query so we don't reset changed entries

--- administrator/components/com_j2store/sql/updates/mysql/4.0.20-2025-12-30.sql
+++ administrator/components/com_j2store/sql/updates/mysql/4.0.20-2025-12-30.sql
@@ -1,6 +1,6 @@
-UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-success text-bg-success' WHERE `orderstatus_name` = 'J2STORE_CONFIRMED';
-UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-info text-bg-info' WHERE `orderstatus_name` = 'J2STORE_PROCESSED';
-UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-important text-bg-danger' WHERE `orderstatus_name` = 'J2STORE_FAILED';
-UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-warning text-bg-warning' WHERE `orderstatus_name` = 'J2STORE_PENDING';
-UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-warning text-bg-warning' WHERE `orderstatus_name` = 'J2STORE_NEW';
-UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-warning text-bg-warning' WHERE `orderstatus_name` = 'J2STORE_CANCELLED';
+UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-success text-bg-success' WHERE `orderstatus_name` = 'J2STORE_CONFIRMED' AND `orderstatus_cssclass` = 'label-success';
+UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-info text-bg-info' WHERE `orderstatus_name` = 'J2STORE_PROCESSED' AND `orderstatus_cssclass` = 'label-info';
+UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-important text-bg-danger' WHERE `orderstatus_name` = 'J2STORE_FAILED' AND `orderstatus_cssclass` = 'label-important';
+UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-warning text-bg-warning' WHERE `orderstatus_name` = 'J2STORE_PENDING' AND `orderstatus_cssclass` = 'label-warning';
+UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-warning text-bg-warning' WHERE `orderstatus_name` = 'J2STORE_NEW' AND `orderstatus_cssclass` = 'label-warning';
+UPDATE `#__j2store_orderstatuses` SET `orderstatus_cssclass` = 'label-warning text-bg-warning' WHERE `orderstatus_name` = 'J2STORE_CANCELLED' AND `orderstatus_cssclass` = 'label-warning';
