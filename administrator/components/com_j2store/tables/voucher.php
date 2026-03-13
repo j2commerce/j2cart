5be4c37c0fa96626b7d1392c8f51bcd018433b41
Olivier <olivier.buisard@simplifyyourweb.com>
2026-03-12 22:38:50 -0400

    Store empty values of valid from and valid to properly

--- administrator/components/com_j2store/tables/voucher.php
+++ administrator/components/com_j2store/tables/voucher.php
@@ -9,6 +9,31 @@ defined('_JEXEC') or die;
 
 class J2storeTableVoucher extends F0FTable
 {
+    /**
+     * Override store method to ensure NULL values are stored in database
+     * instead of '0000-00-00 00:00:00'
+     *
+     * @param   boolean  $updateNulls  True to update fields even if they are null
+     *
+     * @return  boolean  True on success
+     */
+    public function store($updateNulls = false)
+    {
+        $db = $this->getDbo();
+        $nullDate = $db->getNullDate();
+
+        // Convert '0000-00-00 00:00:00' to null before storing
+        if (isset($this->valid_from) && ($this->valid_from === $nullDate || $this->valid_from === '0000-00-00 00:00:00' || empty($this->valid_from))) {
+            $this->valid_from = null;
+        }
+        if (isset($this->valid_to) && ($this->valid_to === $nullDate || $this->valid_to === '0000-00-00 00:00:00' || empty($this->valid_to))) {
+            $this->valid_to = null;
+        }
+
+        // Force update nulls to true so NULL values are actually stored
+        return parent::store(true);
+    }
+
 	/**
 	 * Copy (duplicate) one or more records
 	 *
