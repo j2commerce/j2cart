05201cdb8f9b41ec8c3c392612de387085ef97e7
Olivier <olivier.buisard@simplifyyourweb.com>
2026-08-11 12:20:14 -0400

    Added missing form token

--- plugins/j2store/app_bootstrap4/app_bootstrap4/tmpl/bootstrap4/view_configurableoptions.php
+++ plugins/j2store/app_bootstrap4/app_bootstrap4/tmpl/bootstrap4/view_configurableoptions.php
@@ -264,7 +264,7 @@ $product_id = $this->product->j2store_product_id;
 $('#product-option-<?php echo $option['productoption_id']; ?>').on('click', function() {
 	var node = this;
 	$('#form-upload').remove();
-	$('body').prepend('<form enctype="multipart/form-data" id="form-upload" style="display: none;"><input type="file" name="file" /></form>');
+	$('body').prepend('<form enctype="multipart/form-data" id="form-upload" style="display: none;"><input type="file" name="file" /><input type="hidden" name="<?php echo JSession::getFormToken(); ?>" value="1" /></form>');
 	$('#form-upload input[name=\'file\']').trigger('click');
 	timer = setInterval(function() {
 		if ($('#form-upload input[name=\'file\']').val() != '' && $('#form-upload input[name=\'file\']').val() != undefined) {
