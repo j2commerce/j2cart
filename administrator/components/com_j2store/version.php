a6945545ab10c49a5e90142963f8c46b4a88235d
Olivier <olivier.buisard@simplifyyourweb.com>
2026-08-11 12:52:26 -0400

    Version bump

--- administrator/components/com_j2store/version.php
+++ administrator/components/com_j2store/version.php
@@ -13,7 +13,7 @@
 defined('_JEXEC') or die('Restricted Access');
 
 define('J2STORE_PRO', '1');
-define('J2STORE_VERSION', '4.0.20');
-define('J2STORE_DATE', '2026-01-16');
+define('J2STORE_VERSION', '4.0.21');
+define('J2STORE_DATE', '2026-08-11');
 
 
