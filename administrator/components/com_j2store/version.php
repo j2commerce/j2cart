24a6bd03cfaca84514996dc175b38e7713fb4f7a
Olivier <olivier.buisard@simplifyyourweb.com>
2026-08-19 17:07:08 -0400

    Date bump

--- administrator/components/com_j2store/version.php
+++ administrator/components/com_j2store/version.php
@@ -14,6 +14,6 @@ defined('_JEXEC') or die('Restricted Access');
 
 define('J2STORE_PRO', '1');
 define('J2STORE_VERSION', '4.0.21');
-define('J2STORE_DATE', '2026-08-11');
+define('J2STORE_DATE', '2026-08-18');
 
 
